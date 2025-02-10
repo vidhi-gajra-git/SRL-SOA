97dcfba8001630f2ffc17d7ea64e8ee40c594f1f
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-10 12:49:53 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -6,19 +6,25 @@ from layers import Oper1D
 np.random.seed(10)
 tf.random.set_seed(10)
 
+# Ensure that TensorFlow is using the GPU if available
+if tf.config.list_physical_devices('GPU'):
+    print("GPU is available!")
+else:
+    print("GPU not found. The model will run on CPU.")
+
 ### SLR-OL
 def SLRol(n_bands, q):
-  input = tf.keras.Input((n_bands, 1), name='input')
-  x_0 = Oper1D(n_bands, 3, activation = 'tanh', q = q)(input)
-  y = tf.keras.layers.Dot(axes=(1,1))([x_0, input])
+    with tf.device('/GPU:0'):  # Ensure the model is moved to GPU if available
+        input = tf.keras.Input((n_bands, 1), name='input')
+        x_0 = Oper1D(n_bands, 3, activation='tanh', q=q)(input)
+        y = tf.keras.layers.Dot(axes=(1, 1))([x_0, input])
 
-  model = tf.keras.models.Model(input, y, name='OSEN')
+        model = tf.keras.models.Model(input, y, name='OSEN')
 
-  optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
+        model.compile(optimizer=optimizer, loss='mse')
 
-  model.compile(optimizer = optimizer, loss = 'mse')
+        model.summary()
 
-  model.summary()
-
-  return model
+    return model
