7ac94e5ae25f508315dd825ff3f3ff7314a64013
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-10 13:16:43 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -1,28 +1,24 @@
 import tensorflow as tf
 import numpy as np
-
 from layers import Oper1D
 
 np.random.seed(10)
 tf.random.set_seed(10)
 
-# Ensure that TensorFlow is using the GPU if available
-if tf.config.list_physical_devices('GPU'):
-    print("GPU is available!")
-else:
-    print("GPU not found. The model will run on CPU.")
-
-### SLR-OL
+# SLR-OL Model
 def SLRol(n_bands, q):
-    with tf.device('/GPU:0'):  # Ensure the model is moved to GPU if available
+    # Explicitly place the model on the selected GPU
+    with tf.device('/device:GPU:0'):  # You can use '/device:GPU:1', '/device:GPU:2', etc., as needed
         input = tf.keras.Input((n_bands, 1), name='input')
         x_0 = Oper1D(n_bands, 3, activation='tanh', q=q)(input)
         y = tf.keras.layers.Dot(axes=(1, 1))([x_0, input])
 
         model = tf.keras.models.Model(input, y, name='OSEN')
 
+        # Use GPU for training with Adam optimizer
         optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
+        # Compile the model and move to GPU
         model.compile(optimizer=optimizer, loss='mse')
 
         model.summary()
