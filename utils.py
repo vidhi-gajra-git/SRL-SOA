53b91a7a9c346eb6244535ef425de249aa663917
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-10 14:42:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -97,7 +97,7 @@ def loadData(dataset):
 
     print('\n')
     print('Number of bands: ', str(classData['x_train'].shape[-1]))
-    print(f'======Selected band indices ======= \n {bands_selected}')
+    print(f'======Selected band indices ======= \n {bands_indices}')
 
     return classDataa, Dataa
 
