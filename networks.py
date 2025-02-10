2bc0165b92211040083059115b253d5e1fcb3e2d
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-10 14:03:24 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -10,7 +10,7 @@ tf.random.set_seed(10)
 def SLRol(n_bands, q):
   input = tf.keras.Input((n_bands, 1), name='input')
   x_0 = Oper1D(n_bands, 3, activation = 'tanh', q = q)(input)
-  y = tf.keras.layers.Dot(axes=(1,1))([x_0, input])
+  y = tf.keras.layers.Dot(axes=(2,1))([x_0, input])
 
   model = tf.keras.models.Model(input, y, name='OSEN')
 
