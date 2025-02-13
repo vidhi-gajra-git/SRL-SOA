bffe9c0f760b4ece1802ef5fa728e071a42cb5d9
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-13 18:18:41 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -2,14 +2,17 @@ import tensorflow as tf
 import numpy as np
 
 from layers import Oper1D
-
+from multi_kernel import Oper1DMultiScaleCombined
 np.random.seed(10)
 tf.random.set_seed(10)
 
 ### SLR-OL
 def SLRol(n_bands, q):
   input = tf.keras.Input((n_bands, 1), name='input')
-  x_0 = Oper1D(n_bands, 3, activation = 'tanh', q = q)(input)
+  # x_0 = Oper1D(n_bands, 3, activation = 'tanh', q = q)(input)
+  x_0 =Oper1DMultiScaleCombined(n_bands, 3, activation = 'tanh', q = q)(input)
+  
+  # testing the model on multi-scale conv .... and comparing the o/p 
   y = tf.keras.layers.Dot(axes=(2,1))([x_0, input])
 
   model = tf.keras.models.Model(input, y, name='OSEN')
