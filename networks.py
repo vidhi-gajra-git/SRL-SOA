642abbcae47784520024c58f856e1ba6b711c928
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-14 13:19:12 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -19,7 +19,7 @@ def SLRol(n_bands, q):
 
   # optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
   # Adjust the learning rate
-  optimizer = tf.keras.optimizers.sgd(learning_rate=1e-3,clipvalue=1.0)
+  optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3,clipvalue=1.0)
   model.compile(optimizer=optimizer, loss='mse')
 
 # Add early stopping
