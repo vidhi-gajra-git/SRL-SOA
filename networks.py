8ad953211067246671e6987e4dcce40de405328d
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-17 16:24:25 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from layers import Oper1D
 from multi_kernel import Oper1DMultiScaleCombined
+from multi_layer import Oper1DMultiScaleCombined
 np.random.seed(10)
 tf.random.set_seed(10)
 
@@ -11,7 +12,7 @@ def SLRol(n_bands, q):
   input = tf.keras.Input((n_bands, 1), name='input')
   # x_0 = Oper1D(n_bands, 3, activation = 'tanh', q = q)(input)
   x_0 =Oper1DMultiScaleCombined(n_bands, [3,3,3], activation = 'tanh', q = q)(input)
-  x_0 =Oper1DMultiScaleCombined(n_bands, [3], activation = 'tanh', q = q)(input)
+  # x_0 =Oper1DMultiScaleCombined(n_bands, [3], activation = 'tanh', q = q)(input)
   
   # testing the model on multi-scale conv .... and comparing the o/p 
   y = tf.keras.layers.Dot(axes=(2,1))([x_0, input])
