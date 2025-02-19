efe53602e5c858aa49b8f2bcbd29a9c0be0b95f1
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-19 13:11:31 +0530

    Update multi_layer.py

--- multi_layer.py
+++ multi_layer.py
@@ -50,7 +50,7 @@ class SparseAutoencoderWithAttention(tf.keras.Model):
         x = self.attention_layer([x, x])  # Query = Key = Value
 
         # Sparsity Regularization
-        x = tf.keras.layers.ActivityRegularization(l1=0.01)x)
+        x = tf.keras.layers.ActivityRegularization(l1=0.01)(x)
 
         # Combine and apply activation
         x = self.combine_layer(x)
