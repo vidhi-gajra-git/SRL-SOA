c5b7714c671f1f8c75e67ba5346111e4175b587d
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-19 13:09:52 +0530

    Update multi_layer.py

--- multi_layer.py
+++ multi_layer.py
@@ -11,9 +11,6 @@ class SparseAutoencoderWithAttention(tf.keras.Model):
         self.lambda_ = 0.001
         self.all_layers = {}
 
-        # Self-Attention at the beginning
-        self.attention_layer = tf.keras.layers.Attention()
-
         # Multi-scale Conv1D layers
         for k_size in kernel_sizes:
             layers_for_scale = []
@@ -23,6 +20,9 @@ class SparseAutoencoderWithAttention(tf.keras.Model):
                 )
             self.all_layers[k_size] = layers_for_scale
         
+        # Self-Attention Layer
+        self.attention_layer = tf.keras.layers.Attention()
+
         # Final combining layer
         self.combine_layer = tf.keras.layers.Conv1D(filters, kernel_size=1, padding='same', activation='relu')
 
@@ -34,24 +34,23 @@ class SparseAutoencoderWithAttention(tf.keras.Model):
         def diag_zero(x):
             return tf.linalg.set_diag(x, self.diagonal)
 
-        # Apply Self-Attention at the beginning
-        x = self.attention_layer([input_tensor, input_tensor])  # Query = Key = Value
-
         multi_scale_outputs = []
         for k_size in self.kernel_sizes:
             layers_for_scale = self.all_layers[k_size]
-            x_scale = layers_for_scale[0](x)
+            x_scale = layers_for_scale[0](input_tensor)
             if self.q > 1:
                 for i in range(1, self.q):
-                    x_scale += layers_for_scale[i](tf.math.pow(x, i + 1))
+                    x_scale += layers_for_scale[i](tf.math.pow(input_tensor, i + 1))
             multi_scale_outputs.append(x_scale)
         
         # Concatenating multi-scale outputs
         x = tf.concat(multi_scale_outputs, axis=-1)
 
+        # Apply Self-Attention
+        x = self.attention_layer([x, x])  # Query = Key = Value
+
         # Sparsity Regularization
-        # x = self.sparse_reg(x)
-        x = tf.keras.layers.ActivityRegularization(l1=0.01)(x) 
+        x = tf.keras.layers.ActivityRegularization(l1=0.01)x)
 
         # Combine and apply activation
         x = self.combine_layer(x)
