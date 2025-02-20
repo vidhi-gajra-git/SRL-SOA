ea4bb634ad97b9616f93432adec337eb4c6f707f
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-20 23:40:50 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -11,7 +11,7 @@ tf.random.set_seed(42)
 
 ### SLR-OL
 def SLRol(n_bands, q):
-  # input = tf.keras.Input((n_bands, 1), name='input')
+  input = tf.keras.Input((n_bands, 1), name='input')
   # x_0 = Oper1D(n_bands, 3, activation = 'tanh', q = q)(input)
   # model_name=f'Oper1D_q{q}'
   # hyperparams = Oper1D(n_bands, 3, activation = 'tanh', q = q).get_hyperparameters()
