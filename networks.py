b60e41ab505f268e771709859b8f0adfaf6d09a1
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-21 19:29:22 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -19,10 +19,13 @@ def SLRol(n_bands, q):
  
   # q = 3    # Degree of non-linearity
   num_conv_layers = 2 # Number of Conv1D layers per degree
-  x_0= SparseAutoencoderNonLinear(n=n_bands, q=q, num_conv_layers=num_conv_layers)(input)
-  model_name=f'SparseAutoencoderNonLinear{q}_layers{num_conv_layers}_Xavier_init'
+  # x_0= SparseAutoencoderNonLinear(n=n_bands, q=q, num_conv_layers=num_conv_layers)(input)
+  # model_name=f'SparseAutoencoderNonLinear{q}_layers{num_conv_layers}_Xavier_init'
+  # hyperparams = SparseAutoencoderNonLinear(n=n_bands, q=q, num_conv_layers=num_conv_layers).get_hyperparameters()
+  x_0=MultiKernelEncoder(n=n_bands, q=q, num_conv_layers=num_conv_layers)
+  model_name=f'MultiKernelEncoder{q}_layers{num_conv_layers}_Xavier_init_3_5_7'
   hyperparams = SparseAutoencoderNonLinear(n=n_bands, q=q, num_conv_layers=num_conv_layers).get_hyperparameters()
- 
+
   
   # # print("!!!!!!!!!!",x_0.shape, "!!!!!!!!!!!!!!!!!!!")
   # x_0=SelfONN1D(filters=n_bands, kernel_size=5,q=q)
@@ -33,13 +36,13 @@ def SLRol(n_bands, q):
   y = tf.keras.layers.Dot(axes=(2,1))([x_0, input])
 
   model = tf.keras.models.Model(input, y, name='OSEN')
-  lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-    initial_learning_rate=0.001,
-    decay_steps=10000,
-    decay_rate=0.9
-)
+#   lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+#     initial_learning_rate=0.001,
+#     decay_steps=10000,
+#     decay_rate=0.9
+# )
 
-  optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+  optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
   # optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 #   # Adjust the learning rate
 #   optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
