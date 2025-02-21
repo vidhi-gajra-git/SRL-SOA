b0a8d3e7c2e00ff7b9dce448adb0513fa702fc99
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-21 15:10:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('GPU')[
 np.random.seed(42)
 tf.random.set_seed(42)
 np.random.seed(42)
-np.random.set_seed(42)
+# np.random.set_seed(42)
 ap = argparse.ArgumentParser()
 ap.add_argument('--method', default='SRL-SOA', help =
                 "SRL-SOA, PCA, SpaBS, EGCSR_R, ISSC, None (for no band selection).")
