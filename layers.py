65cb57c9c7bcce2bf94f017e5c1736f8489a0900
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-22 15:50:11 +0530

    Update layers.py

--- layers.py
+++ layers.py
@@ -14,7 +14,7 @@ class Oper1D(tf.keras.Model):
     for i in range(0, q):  # q convolutional layers.
       self.all_layers.append(tf.keras.layers.Conv1D(filters,
                                                     kernel_size,
-                                                    padding='same', activation=None))
+                                                    padding='same', activation=activation,  kernel_initializer=tf.keras.initializers.GlorotNormal() ))
   def get_hyperparameters(self):
         return {k: v for k, v in self.__dict__.items() if isinstance(v, (int, float, str, bool))}
 
