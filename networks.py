e557c1af69ff44505fc6e451aa0f5a37fab81b1d
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-28 00:03:20 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -13,15 +13,15 @@ tf.random.set_seed(42)
 ### SLR-OL
 def SLRol(n_bands, q):
   input = tf.keras.Input((n_bands, 1), name='input')
-  # x_0 = Oper1D(n_bands, 3, activation = 'tanh', q = q)(input)
-  # model_name=f'Oper1D_q{q}'
-  # hyperparams = Oper1D(n_bands, 3, activation = 'tanh', q = q).get_hyperparameters()
+  x_0 = Oper1D(n_bands, 3, activation = 'tanh', q = q)(input)
+  model_name=f'Oper1D_q{q}'
+  hyperparams = Oper1D(n_bands, 3, activation = 'tanh', q = q).get_hyperparameters()
  
   # q = 3    # Degree of non-linearity
-  num_conv_layers = 2 # Number of Conv1D layers per degree
-  x_0= SparseAutoencoderNonLinear(n=n_bands, q=q, num_conv_layers=num_conv_layers)(input)
-  model_name=f'SparseAutoencoderNonLinear{q}_layers{num_conv_layers}_Xavier_60_Epochs'
-  hyperparams = SparseAutoencoderNonLinear(n=n_bands, q=q, num_conv_layers=num_conv_layers).get_hyperparameters()
+  # num_conv_layers = 2 # Number of Conv1D layers per degree
+  # x_0= SparseAutoencoderNonLinear(n=n_bands, q=q, num_conv_layers=num_conv_layers)(input)
+  # model_name=f'SparseAutoencoderNonLinear{q}_layers{num_conv_layers}_Xavier_60_Epochs'
+  # hyperparams = SparseAutoencoderNonLinear(n=n_bands, q=q, num_conv_layers=num_conv_layers).get_hyperparameters()
   # x_0=MultiKernelEncoder(n=n_bands, q=q, num_conv_layers=num_conv_layers)(input)
   # model_name=f'MultiKernelEncoder{q}_layers{num_conv_layers}_Xavier_init_3_5_7'
   # hyperparams = MultiKernelEncoder(n=n_bands, q=q, num_conv_layers=num_conv_layers).get_hyperparameters()
