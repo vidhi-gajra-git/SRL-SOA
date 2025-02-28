485772fcffa9f014ae81970be51a1788611ca356
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-02-28 14:20:09 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -29,12 +29,23 @@ class WarmUpExponentialDecay(tf.keras.optimizers.schedules.LearningRateSchedule)
 ### SLR-OL
 def SLRol(n_bands, q):
   input = tf.keras.Input((n_bands, 1), name='input')
+
+
+# Load the pretrained guiding classifier
+  guiding_classifier = tf.keras.models.load_model("guiding_classifier.h5")
+
+# Ensure the classifier is not trainable
+  guiding_classifier.trainable = False  
+
+# Print model summary to verify
+  guiding_classifier.summary()
+
   # x_0 = Oper1D(n_bands, 3, activation = 'tanh', q = q)(input)
   # model_name=f'Oper1D_q{q}'
   # hyperparams = Oper1D(n_bands, 3, activation = 'tanh', q = q).get_hyperparameters()
  
   # q = 3    # Degree of non-linearity
-  num_conv_layers = 4 # Number of Conv1D layers per degree
+  num_conv_layers = 2 # Number of Conv1D layers per degree
   x_0= SparseAutoencoderNonLinear(n=n_bands, q=q, num_conv_layers=num_conv_layers)(input)
   model_name=f'SparseAutoencoderNonLinear{q}_layers{num_conv_layers}_Xavier_Learning_RateScehduled'
   hyperparams = SparseAutoencoderNonLinear(n=n_bands, q=q, num_conv_layers=num_conv_layers).get_hyperparameters()
@@ -50,35 +61,38 @@ def SLRol(n_bands, q):
   
   # testing the model on multi-scale conv .... and comparing the o/p 
   y = tf.keras.layers.Dot(axes=(2,1))([x_0, input])
+  class_preds = guiding_classifier(y)
 
-  model = tf.keras.models.Model(input, y, name=model_name)
+# Define the combined model
+  combined_model = tf.keras.Model(inputs=inputs, outputs=[y, class_preds], name='GuidedSparseAutoencoder')
+  model_name=f'SCombinedModel{q}_layers{num_conv_layers}_Xavier'
+  
+
+# Compile the model
+
+# Print summary
+  combined_model.summary()
+
+  # model = tf.keras.models.Model(input, y, name=model_name)
+  
 #   lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
 #     initial_learning_rate=0.001,
 #     decay_steps=10000,
 #     decay_rate=0.9
 # )
-  hyperparams2 = {
-        "n_bands": n_bands,
-        "q": q,
-        "num_conv_layers": num_conv_layers,
-        "activation": "tanh",
-        "lambda_l1": 0.01,
-        "initial_lr": 0.001,
-        "warmup_steps": 1000,
-        "decay_steps": 10000,
-        "decay_rate": 0.9,
-        "sparsity_warmup_epochs": 10
-    }
-  lr_schedule = WarmUpExponentialDecay(initial_lr=hyperparams2["initial_lr"],
-                                         warmup_steps=hyperparams2["warmup_steps"],
-                                         decay_steps=hyperparams2["decay_steps"],
-                                         decay_rate=hyperparams2["decay_rate"])
 
-  optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule )
-  # optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+  # lr_schedule = WarmUpExponentialDecay(initial_lr=hyperparams2["initial_lr"],
+  #                                        warmup_steps=hyperparams2["warmup_steps"],
+  #                                        decay_steps=hyperparams2["decay_steps"],
+  #                                        decay_rate=hyperparams2["decay_rate"])
+
+  # optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule )
+  optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 #   # Adjust the learning rate
 #   optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
-  model.compile(optimizer=optimizer,loss='mse')
+  # model.compile(optimizer=optimizer,loss='mse')
+  combined_model.compile(optimizer=optimizer, loss=['mse', 'categorical_crossentropy'], loss_weights=[1.0, 0.2])
+  
 
 # # Add early stopping
   
@@ -89,4 +103,4 @@ def SLRol(n_bands, q):
 
   model.summary()
     
-  return model_name, hyperparams , model
+  return model_name, hyperparams , combined_model
