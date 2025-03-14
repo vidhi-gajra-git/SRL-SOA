abb8a07dd7ed901c490c3a598d4251536cb0f17a
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-03-14 20:28:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -178,7 +178,7 @@ def loadData(dataset):
     Dataa = []
     for _ in range(10):  # No need to vary seed in loop
         x_train, x_test, y_train, y_test = train_test_split(
-            x_class, y_class, test_size=0.95, random_state=42
+            x_class, y_class, test_size=0.85, random_state=42
         )
 
         # Use the global scaler
