110c63c07a0c96dcb68e9c13cb0c169f77ba64ed
vidhi-gajra-git <129372027+vidhi-gajra-git@users.noreply.github.com>
2025-03-20 19:05:13 +0530

    Update networks.py

--- networks.py
+++ networks.py
@@ -44,7 +44,9 @@ def SLRol(n_bands, q):
   def sparse_loss(y_true, y_pred):
         sparsity_level = 0.01
         mse_loss = tf.math.reduce_mean(tf.keras.losses.MSE(y_true, y_pred))
-        mean_activation = tf.math.reduce_mean(x_0, axis=0)  # Use the existing output
+        x_0 = tf.convert_to_tensor(x_0, dtype=tf.float32)
+        mean_activation = K.mean(x_0, axis=1, keepdims=True)
+        # mean_activation = tf.math.reduce_mean(x_0, axis=0)  # Use the existing output
 
         kl_divergence = tf.reduce_sum(sparsity_level * tf.math.log(sparsity_level / (mean_activation + 1e-10)) +
                                       (1 - sparsity_level) * tf.math.log((1 - sparsity_level) / (1 - mean_activation + 1e-10)))
